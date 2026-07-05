38b875908a5f3c516cd4cecd3d06c6ba639468f3
Simon Lau <simon.lau.sg@gmail.com>
2026-07-05 10:40:53 +0800

    feat: did ex06

---
+++ ex06/ft_str_is_printable.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/05 09:48:03 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/05 10:39:43 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_printable(char *str)
+{
+	char *letter_ptr = str;
+
+	while (*letter_ptr != '\0')
+	{
+		if (*letter_ptr < ' ' || *letter_ptr > '~')
+		{
+			return (0);
+		}
+		letter_ptr++;
+	}
+	return (1);
+}
---
+++ ex06/test.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/05 09:48:10 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/05 10:40:42 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <assert.h>
+
+int		ft_str_is_printable(char *str);
+
+void	test_only_printable(void)
+{
+	char	*s;
+	int		result;
+
+	s = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	result = ft_str_is_printable(s);
+	assert(1 == result);
+}
+
+void	test_invalid(void)
+{
+	char	*s;
+	int		result;
+
+	s = "!\"#$%&'()*+,-./0123456789\n:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	result = ft_str_is_printable(s);
+	assert(0 == result);
+}
+
+void	test_empty(void)
+{
+	char	*s;
+	int		result;
+
+	s = "";
+	result = ft_str_is_printable(s);
+	assert(1 == result);
+}
+
+int	main(void)
+{
+	test_only_printable();
+	test_invalid();
+	test_empty();
+}
