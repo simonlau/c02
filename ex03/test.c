6ba3c9023b9cf65889830833c2d3fd52b8ead888
Simon Lau <simon.lau.sg@gmail.com>
2026-07-05 09:46:13 +0800

    feat: did ex03

---
+++ ex03/test.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/05 09:36:19 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/05 09:44:11 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <assert.h>
+
+int		ft_str_is_numeric(char *str);
+
+void	test_only_digits(void)
+{
+	char	*s;
+	int		result;
+
+	s = "0123456789";
+	result = ft_str_is_numeric(s);
+	assert(1 == result);
+}
+
+void	test_invalid(void)
+{
+	char	*s;
+	int		result;
+
+	s = "012345 6789";
+	result = ft_str_is_numeric(s);
+	assert(0 == result);
+}
+
+void	test_empty(void)
+{
+	char	*s;
+	int		result;
+
+	s = "";
+	result = ft_str_is_numeric(s);
+	assert(1 == result);
+}
+
+int	main(void)
+{
+	test_only_digits();
+	test_invalid();
+	test_empty();
+}
