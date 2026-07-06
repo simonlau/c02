64286b47f68ee1f1710206b531fcd7f318893234
Choo Leng Simon Lau <choolau@c2r3s9.42singapore.sg>
2026-07-06 20:42:16 +0800

    fix: fix ex00 test

--- ex00/test.c
+++ ex00/test.c
@@ -6,7 +6,7 @@
 /*   By: choolau <choolau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 17:22:49 by choolau           #+#    #+#             */
-/*   Updated: 2026/07/04 17:34:35 by choolau          ###   ########.fr       */
+/*   Updated: 2026/07/06 20:42:00 by choolau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ char	*ft_strcpy(char *dest, char *src);
 
 int	main(void)
 {
-	char	a[] = "simon";
-	char	b[] = "12345";
+	char	dest[] = "simon";
+	char	src[] = "12345";
 	char	*result;
+	char	*expected;
 
-	result = ft_strcpy(a, b);
-	assert(strcmp(a, b) == 0);
+	expected = "12345";
+	result = ft_strcpy(dest, src);
+	assert(strcmp(result, expected) == 0);
 }
