1b8ea7afa6b2e16bb199078de2dbb4f42b83fcd1
Simon Lau <simon.lau.sg@gmail.com>
2026-07-12 15:28:06 +0800

    test: add more tests for strcpy

--- ex00/test.c
+++ ex00/test.c
@@ -1,29 +1,79 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: choolau <choolau@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/04 17:22:49 by choolau           #+#    #+#             */
-/*   Updated: 2026/07/06 20:42:00 by choolau          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
 char	*ft_strcpy(char *dest, char *src);
 
-int	main(void)
+void	test_normal(void)
 {
 	char	dest[] = "simon";
-	char	src[] = "12345";
 	char	*result;
-	char	*expected;
 
-	expected = "12345";
-	result = ft_strcpy(dest, src);
-	assert(strcmp(result, expected) == 0);
+	result = ft_strcpy(dest, "12345");
+	assert(dest == result);
+	assert(strcmp("12345", result) == 0);
+}
+
+void	test_empty(void)
+{
+	char	dest[] = "simon";
+	char	*result;
+
+	result = ft_strcpy(dest, "");
+	assert(dest == result);
+	assert(strcmp("", result) == 0);
+}
+
+void	test_src_short(void)
+{
+	char	dest[] = "simons";
+	char	*result;
+
+	result = ft_strcpy(dest, "123");
+	assert(dest == result);
+	assert(strcmp("123", result) == 0);
+	assert(dest[4] == 'n');
+	assert(dest[5] == 's');
+}
+
+void	test_src_single(void)
+{
+	char	dest[] = "simons";
+	char	*result;
+
+	result = ft_strcpy(dest, "1");
+	assert(dest == result);
+	assert(strcmp("1", result) == 0);
+	assert(dest[2] == 'm');
+	assert(dest[3] == 'o');
+}
+
+void	test_src_empty(void)
+{
+	char	dest[] = "simons";
+	char	*result;
+
+	result = ft_strcpy(dest, "");
+	assert(dest == result);
+	assert(strcmp("", result) == 0);
+}
+
+void	test_src_dest_same(void)
+{
+	char	dest[] = "simon";
+	char	*result;
+
+	result = ft_strcpy(dest, dest);
+	assert(dest == result);
+	assert(strcmp("simon", result) == 0);
+}
+
+int	main(void)
+{
+	test_normal();
+	test_empty();
+	test_src_short();
+	test_src_single();
+	test_src_empty();
+	test_src_dest_same();
 }
