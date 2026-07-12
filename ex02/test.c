248d5c136dd84e83fe1645ad05629d49fb220a05
Simon Lau <simon.lau.sg@gmail.com>
2026-07-12 15:43:38 +0800

    test: add more tests for is_alpha

--- ex02/test.c
+++ ex02/test.c
@@ -3,46 +3,42 @@
 
 int		ft_str_is_alpha(char *str);
 
-int	random_num(int low, int high)
-{
-	int	range;
-
-	range = high - low;
-	return ((rand() % range) + low);
-}
-
 void	test_valid_lower(void)
 {
-	char	x[] = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-
-	assert(ft_str_is_alpha(x));
+	assert(1 == ft_str_is_alpha("G"));
+	assert(1 == ft_str_is_alpha("ABCDEFGHIJKLMOPQRSTUVWXYZ"));
 }
 
 void	test_valid_upper(void)
 {
-	char	x[] = "abcdefghijklmnopqrstuvwxyz";
+	assert(1 == ft_str_is_alpha("g"));
+	assert(1 == ft_str_is_alpha("abcdefghijklmnopqrstuvwxyz"));
+}
 
-	assert(ft_str_is_alpha(x));
+void	test_valid_mixed(void)
+{
+	assert(1 == ft_str_is_alpha("gAh"));
+	assert(1 == ft_str_is_alpha("KKaaLLmm"));
 }
 
 void	test_empty(void)
 {
-	char	x[] = "";
-
-	assert(ft_str_is_alpha(x) == 1);
+	assert(1 == ft_str_is_alpha(""));
 }
 
 void	test_invalid(void)
 {
-	char	x[] = "abcdefgh_s";
-
-	assert(ft_str_is_alpha(x) == 0);
+	assert(0 == ft_str_is_alpha("?"));
+	assert(0 == ft_str_is_alpha("abcde_fghs"));
+	assert(0 == ft_str_is_alpha("1abcdefgh"));
+	assert(0 == ft_str_is_alpha("abcdefgh9"));
 }
 
 int	main(void)
 {
 	test_valid_upper();
 	test_valid_lower();
+	test_valid_mixed();
 	test_empty();
 	test_invalid();
 }
